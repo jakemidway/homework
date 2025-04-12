85ec52004394aa429232ecce9692262045704d80
jakemidway <jakemidway@gmail.com>
2025-04-11 23:40:14 -0400

    sd

---
+++ src/HW/1/UserList.tsx
@@ -0,0 +1,33 @@
+type AddressType = {
+    street: string// ПОДПРАВЛЯЕМ any
+    city: string // ПОДПРАВЛЯЕМ any
+};
+
+export type UserType = {
+    id: number
+    name: string
+    age: number
+    address: AddressType
+};
+
+type UserListPropsType = {
+    users: Array<UserType>; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
+};
+
+export const UserList = (props: UserListPropsType) => {
+    return (
+        <div id={'hw01-users'}>
+            <h2>User List:</h2>
+            console.log(2242)
+            <ul>
+                {props.users.map((user, index) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
+                    <li key={index} id={`hw01-user-${user.id}`}>
+                        <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
+
+                        {user.address.street}, {user.address.city}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
